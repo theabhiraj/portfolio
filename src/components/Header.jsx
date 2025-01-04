afb04870a57d62221c34b9a5c4b050203b993391
Abhiraj <109774987+theabhiraj@users.noreply.github.com>
2025-01-04 15:56:56 +0530

    links fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,7 +41,7 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
-        <a href="/" className="logo">
+        <a href="" className="logo">
           <span className="logo-text">Abhiraj R.</span>
         </a>
 
